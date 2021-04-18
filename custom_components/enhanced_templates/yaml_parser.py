d9d1038098036a62c5094f9bc1a2b58b5ef0b5ce
Jason Knott <24280037+jayknott@users.noreply.github.com>
2021-04-18 15:28:51 -0700

    update

--- custom_components/enhanced_templates/yaml_parser.py
+++ custom_components/enhanced_templates/yaml_parser.py
@@ -119,7 +119,7 @@ def _include_dir_list_yaml(
     node_values = process_node(loader, node)
     loc: str = os.path.join(os.path.dirname(loader.name), node_values[0])
     return [
-        load_yaml(f, None, node_values[1])
+        load_yaml(f, None, node_values[2])
         for f in hass_loader._find_files(loc, "*.yaml")
         if os.path.basename(f) != hass_loader.SECRET_YAML
     ]
@@ -136,7 +136,7 @@ def _include_dir_merge_list_yaml(
     for fname in hass_loader._find_files(loc, "*.yaml"):
         if os.path.basename(fname) == hass_loader.SECRET_YAML:
             continue
-        loaded_yaml = load_yaml(fname, None, node_values[1])
+        loaded_yaml = load_yaml(fname, None, node_values[2])
         if isinstance(loaded_yaml, list):
             merged_list.extend(loaded_yaml)
     return hass_loader._add_reference(merged_list, loader, node)
@@ -154,7 +154,7 @@ def _include_dir_named_yaml(
         filename = os.path.splitext(os.path.basename(fname))[0]
         if os.path.basename(fname) == hass_loader.SECRET_YAML:
             continue
-        mapping[filename] = load_yaml(fname, None, node_values[1])
+        mapping[filename] = load_yaml(fname, None, node_values[2])
     return hass_loader._add_reference(mapping, loader, node)
 
 
